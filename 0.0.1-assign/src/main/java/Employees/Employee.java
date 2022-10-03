2c895f38b7f0442d8bceb54963a0d9ded3023d40
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-10-03 15:15:08 +0530

    commit for Employee Map

--- 0.0.1-assign/src/main/java/Employees/Employee.java
+++ 0.0.1-assign/src/main/java/Employees/Employee.java
@@ -26,6 +26,5 @@ System.out.println(hm);
 	
 
 }
+
 }
-
-
---
+++ 0.0.1-assign/src/test/java/Employees/EmployeeTest.java
@@ -0,0 +1,19 @@
+package Employees;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class EmployeeTest {
+	@SuppressWarnings("deprecation")
+	@Test
+	public void Employee() {
+		Employee emp = new Employee();
+ 	
+	    Assert.assertNotNull("Provided Map is full", emp);
+	
+	    	 
+	}
+}
