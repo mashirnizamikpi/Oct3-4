0a93fe6bbf2e38c2ea415f9114e72a4b3242474b
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-10-03 10:48:53 +0530

    Commit for Employee-Map

---
+++ 0.0.1-assign/src/main/java/Employees/Employee.java
@@ -0,0 +1,31 @@
+package Employees;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Employee{
+	
+	public static void main(String[] args) {
+		
+
+	System.out.println("Employees = EmpId");
+
+	//when we using HashMap for two parameters at one time
+Map<String,Integer> hm = new HashMap<String, Integer>();
+
+hm.put("Amit", 9192);
+hm.put("Ashok", 9193);
+hm.put("Brishej", 9194);
+hm.put("Chander", 9195);
+hm.put("Mashir", 9196);
+hm.put("Peter", 9197);
+System.out.println(hm);
+
+	
+	
+
+}
+}
+
+
