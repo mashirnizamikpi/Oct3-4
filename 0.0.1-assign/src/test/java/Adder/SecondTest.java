2c9909e6936457e45e089ffadb918b850ea28416
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-10-03 14:40:42 +0530

    Commit for Method Overloading

---
+++ 0.0.1-assign/src/test/java/Adder/SecondTest.java
@@ -0,0 +1,20 @@
+package Adder;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SecondTest {
+
+	@Test
+	public void First() {
+		First f = new First();// Creating the object of the class "c"
+		int q = f.add(1000,100);//Getting method through 
+		assertEquals(1100,q);
+		
+		int d = f.add(1000,1000,2000);//Getting method through 
+		assertEquals(4000,d);
+		
+	}
+
+}
